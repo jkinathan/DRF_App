67c3f61af45efd4aad007463f829e42abbc269a3
jkinathan <jkinobe@gmail.com>
2021-06-02 12:50:48 +0300

    JWT authentication

--- myApiProject/gateway/models.py
+++ myApiProject/gateway/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from user.models import CustomUser
 
 # Create your models here.
+class JWT(models.Model):
+    user = models.ForeignKey(CustomUser, related_name="login_user", on_delete=models.CASCADE)
+    access = models.TextField()
+    refresh = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)   
--- myApiProject/gateway/views.py
+++ myApiProject/gateway/views.py
@@ -1,3 +1,57 @@
+from random import random
+from django.http import response
+
+from rest_framework import serializers
+from DRF_App.myApiProject.myApiProject.settings import SECRET_KEY
 from django.shortcuts import render
+from .models import JWT
+from user.models import CustomUser
+import Jwt
+from datetime import datetime, timedelta
+from django.conf import settings
+import random
+import string
+from rest_framework.views import APIView
+from .serializers import LoginSerializer
+from django.contrib.auth import authenticate
+from rest_framework .response import Response
 
 # Create your views here.
+def get_random(length):
+    ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+def get_access_token(payload):
+    return Jwt.encode(
+        {"exp": datetime.now()+timedelta(minutes=5),**payload},
+            settings.SECRET_KEY,
+            algorithm="HS256"
+    ) 
+
+def get_refresh_token():
+    return Jwt.encode(
+        {"exp": datetime.now()+timedelta(days=365),"data":get_random(10)},
+            settings.SECRET_KEY,
+            algorithm="HS256"
+    )
+
+class LoginView(APIView):
+
+    serializer_class = LoginSerializer
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = authenticate(username=serializer.validated_data['email'],
+                            password=serializer.validated_data['password'])
+        
+        if not user:
+            return Response({"error":"Invalid email or Password"}, status="400")
+
+        access = get_access_token({"user_id": user.id})
+        refresh = get_refresh_token()
+
+        Jwt.objects.create(
+            user_id = user.id , access=access, refresh=refresh
+        )
+
+        return response({"access":access,"refresh": refresh})
