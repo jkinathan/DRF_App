6c4218bcc32fa26e2256cc31094f0cfb7ec4012a
jkinathan <jkinobe@gmail.com>
2021-06-03 11:42:22 +0300

    authorization2

--- myApiProject/gateway/views.py
+++ myApiProject/gateway/views.py
@@ -2,33 +2,33 @@ from random import random
 from django.http import response
 
 from rest_framework import serializers
-from DRF_App.myApiProject.myApiProject.settings import SECRET_KEY
+from myApiProject.settings import SECRET_KEY
 from django.shortcuts import render
 from .models import JWT
 from user.models import CustomUser
-import Jwt
+import jwt
 from datetime import datetime, timedelta
 from django.conf import settings
 import random
 import string
 from rest_framework.views import APIView
-from .serializers import LoginSerializer
+from .serializers import LoginSerializer, RegisterSerializer
 from django.contrib.auth import authenticate
 from rest_framework .response import Response
 
 # Create your views here.
 def get_random(length):
-    ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
 
 def get_access_token(payload):
-    return Jwt.encode(
+    return jwt.encode(
         {"exp": datetime.now()+timedelta(minutes=5),**payload},
             settings.SECRET_KEY,
             algorithm="HS256"
     ) 
 
 def get_refresh_token():
-    return Jwt.encode(
+    return jwt.encode(
         {"exp": datetime.now()+timedelta(days=365),"data":get_random(10)},
             settings.SECRET_KEY,
             algorithm="HS256"
@@ -50,8 +50,20 @@ class LoginView(APIView):
         access = get_access_token({"user_id": user.id})
         refresh = get_refresh_token()
 
-        Jwt.objects.create(
+        JWT.objects.create(
             user_id = user.id , access=access, refresh=refresh
         )
 
-        return response({"access":access,"refresh": refresh})
+        return Response({"access":access,"refresh": refresh})
+
+class RegisterView(APIView):
+
+    serializer_class = RegisterSerializer
+
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        CustomUser.objects._create_user(**serializer.validated_data)
+
+        return Response({"success":"User Created."})
